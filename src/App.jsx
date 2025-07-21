83c20ef80019aded7d7dcefa37e6d03efe609bcd
Sandeepsaini123 <sandeepsaininnl0@gmail.com>
2025-07-21 11:55:16 +0530

    nav update

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Footer from './Components/Footer';
 
 const App = () => {
   const [category, setCategory] = useState('general');
+
   return (
     <div>
       <Navbar setCategory={setCategory} />
