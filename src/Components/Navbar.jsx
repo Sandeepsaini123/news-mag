56b08db4827677c30f6ca460b5039d03e15911c5
Sandeepsaini123 <sandeepsaininnl0@gmail.com>
2025-07-21 17:54:54 +0530

    nav update

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,33 +1,35 @@
-const Navbar = ({ setCategory, activeCategory, setActiveCategory }) => {
-  const handleClick = (category) => {
-    setCategory(category);
-    setActiveCategory(category);
-  };
-
+const Navbar = ({ setCategory }) => {
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
-      <div className="container-fluid">
-        <a className="navbar-brand" href="#"><span className="badge bg-light text-dark">NewsMag</span></a>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            {['technology', 'business', 'health', 'science', 'sports', 'entertainment'].map(category => (
-              <li className="nav-item" key={category}>
-                <div
-                  className={`nav-link nav-link-hover ${activeCategory === category ? 'active-category' : ''}`}
-                  onClick={() => handleClick(category)}
-                >
-                  {category.charAt(0).toUpperCase() + category.slice(1)}
-                </div>
-              </li>
-            ))}
-          </ul>
-        </div>
-      </div>
-    </nav>
-  );
-};
-
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#"><span className="badge bg-light text-dark">NewsMag</span></a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <div className="nav-link active" style={{ cursor: 'pointer' }} onClick={() => setCategory('technology')}>Technology</div>
+        </li>
+          <li className="nav-item">
+          <div className="nav-link active" style={{ cursor: 'pointer' }} onClick={() => setCategory('business')}>Business</div>
+        </li>
+         <li className="nav-item">
+          <div className="nav-link active" style={{ cursor: 'pointer' }} onClick={() => setCategory('health')}>Health</div>
+        </li>
+         <li className="nav-item">
+          <div className="nav-link active" style={{ cursor: 'pointer' }} onClick={() => setCategory('science')}>Science</div>
+        </li>
+         <li className="nav-item">
+          <div className="nav-link active" style={{ cursor: 'pointer' }} onClick={() => setCategory('sports')}>Sports</div>
+        </li>
+        <li className="nav-item">
+          <div className="nav-link active" style={{ cursor: 'pointer' }} onClick={() => setCategory('entertainment')}>Entertainment</div>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+)
+}
 export default Navbar;
